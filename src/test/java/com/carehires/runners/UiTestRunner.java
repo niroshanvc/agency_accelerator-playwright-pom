b9bd617bccd5b0bb285f5d6da3ba52b798bc4790
Niroshan - CareHires <niroshan.desilva@carehires.com>
2025-12-24 12:29:12 +0530

    Add separate Cucumber runners for API and UI tests

--- src/test/java/com/carehires/runners/UiTestRunner.java
+++ src/test/java/com/carehires/runners/UiTestRunner.java
@@ -1,17 +1,23 @@
 package com.carehires.runners;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
-
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.carehires.steps,com.carehires.hooks")
-@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@UI")
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/features/ui",
+    glue = {"com.carehires.steps", "com.carehires.hooks"},
+    tags = "@ui",
+    plugin = {
+        "pretty",
+        "html:reports/cucumber-reports/ui-report.html",
+        "json:reports/cucumber-reports/ui-report.json",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+    },
+    monochrome = true,
+    dryRun = false
+)
 public class UiTestRunner {
 }
